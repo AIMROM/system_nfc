24a668d5b41c18c8c45c12e7c39dad79081defc5
Ruchi Kandoi <kandoiruchi@google.com>
2017-08-01 16:26:17 -0700

    Fix NXP_CHIP_TYPE and compile errors
    NXP_CHIP_TYPE should be set in the device.mk for respective device and
    not hard-coded. Fix compile time errors for the same.
    
    Bug: 64264750
    Test: HCE-F Emulator/Reader CTS tests
    Change-Id: I6dc55d319784be0bc2b54d4801ac1f1695ebbe7e
    (cherry picked from commit 6f1cd4d771e6670fb8b99eca41d171ba7f95adf2)
    (cherry picked from commit 615a7be8d8d481302071e9d2faeb278c20fcf42b)

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -35,17 +35,6 @@ ifeq ($(PN553),4)
 D_CFLAGS += -DPN553=4
 endif
 
-#### Select the CHIP ####
-ifneq ($(filter pn547 nq110 nq120,$(BOARD_NFC_CHIPSET)),)
-NXP_CHIP_TYPE := $(PN547C2)
-else ifneq ($(filter pn548 nq210 nq220,$(BOARD_NFC_CHIPSET)),)
-NXP_CHIP_TYPE := $(PN548C2)
-else ifeq ($(BOARD_NFC_CHIPSET),pn551)
-NXP_CHIP_TYPE := $(PN551)
-else ifeq ($(BOARD_NFC_CHIPSET),pn553)
-NXP_CHIP_TYPE := $(PN553)
-endif
-
 ifeq ($(NXP_CHIP_TYPE),$(PN547C2))
 D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN547C2
 else ifeq ($(NXP_CHIP_TYPE),$(PN548C2))
